52c114a883af29d9bf007627ee13b599b636042b
AliDurul <alidrl26@gmail.com>
2025-07-14 10:12:48 +0200

    Remove unused SVG files and add TypeScript type definitions for companions and messaging system

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button"
+
+
+const Page = async () => {
+  // const companions = await getAllCompanions({ limit: 3 });
+  // const recentSessionsCompanions = await getRecentSessions(10);
+
+  return (
+    <main>
+      <h1 className="text-red-600">Popular Companions</h1>
+      <Button>
+        hi
+      </Button>
+    </main>
+  )
+}
+
+export default Page
